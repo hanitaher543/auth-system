ddcf3b36d16452be1cbf9fb05f77cbb81b9e10bd
hanitaher0106 <hani.taher2000@gmail.com>
2024-08-24 17:00:52 +0100

    fix: resolve Docker issue with bcrypt module

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -5,4 +5,5 @@ module.exports = async function (fastify) {
     fastify.post('/register', createUser);
     fastify.post('/login', login);
     fastify.post('/logout', logout);
+    fastify.get('/ping',(req, res)=>res.send({message : 'Backend Work !'}))
 };
